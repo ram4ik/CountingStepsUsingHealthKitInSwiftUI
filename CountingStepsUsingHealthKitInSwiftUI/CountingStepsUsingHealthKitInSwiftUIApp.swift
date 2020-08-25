2df355626bafeab6fd16ed66fb823f598ccb974b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-25 12:21:39 +0300

    Initial Commit

---
+++ CountingStepsUsingHealthKitInSwiftUI/CountingStepsUsingHealthKitInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountingStepsUsingHealthKitInSwiftUIApp.swift
+//  CountingStepsUsingHealthKitInSwiftUI
+//
+//  Created by ramil on 25.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct CountingStepsUsingHealthKitInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
